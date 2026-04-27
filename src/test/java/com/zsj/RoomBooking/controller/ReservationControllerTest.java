d966d0e63e9df534874d797d044cee928e90a48f
zsjgithub654 <zsj654@gmail.com>
2026-04-27 15:16:32 -0700

    Reservation controller: improve test.

--- src/test/java/com/zsj/RoomBooking/controller/ReservationControllerTest.java
+++ src/test/java/com/zsj/RoomBooking/controller/ReservationControllerTest.java
@@ -15,8 +15,8 @@ import org.springframework.test.web.servlet.MockMvc;
 import tools.jackson.core.type.TypeReference;
 import tools.jackson.databind.ObjectMapper;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -42,71 +42,77 @@ public class ReservationControllerTest {
 
     @Test
     void searchReservationsTest() throws Exception {
-        List<ReservationResponse> serviceResponse = new ArrayList<>();
-        serviceResponse.add(new ReservationResponse(0L, 10L, 20L, LocalDateTime.of(2026, 3, 1, 10, 0, 0, 0), LocalDateTime.of(2026, 3, 1, 10, 30, 0, 0), ReservationStatus.RESERVATION_STATUS_ACTIVE));
-        serviceResponse.add(new ReservationResponse(1L, 11L, 20L, LocalDateTime.of(2026, 3, 1, 14, 30, 0, 0), LocalDateTime.of(2026, 3, 1, 15, 30, 0, 0), ReservationStatus.RESERVATION_STATUS_CANCELED));
-        serviceResponse.add(new ReservationResponse(2L, 11L, 21L, LocalDateTime.of(2026, 3, 3, 10, 0, 0, 0), LocalDateTime.of(2026, 3, 3, 12, 0, 0, 0), ReservationStatus.RESERVATION_STATUS_ACTIVE));
-
+        /* request */
+        Long userId = 0L;
+        Long roomId = 10L;
+        LocalDate date = LocalDate.of(2026, 3, 1);
+        /* mock service response */
+        List<ReservationResponse> serviceResponse = List.of(
+                new ReservationResponse(0L, 10L, 20L,
+                        LocalDateTime.of(2026, 3, 1, 10, 0, 0, 0),
+                        LocalDateTime.of(2026, 3, 1, 10, 30, 0, 0),
+                        ReservationStatus.RESERVATION_STATUS_ACTIVE),
+                new ReservationResponse(1L, 11L, 20L,
+                        LocalDateTime.of(2026, 3, 1, 14, 30, 0, 0),
+                        LocalDateTime.of(2026, 3, 1, 15, 30, 0, 0),
+                        ReservationStatus.RESERVATION_STATUS_CANCELED));
         when(reservationService.searchReservations(any(SearchReservationRequest.class))).thenReturn(serviceResponse);
 
-        /* to compare LocalDateTime, parse response to dto object */
-        String responseString = mockMvc.perform(get("/reservations"))
+        /* perform, to compare LocalDateTime, need to parse response to dto object */
+        String responseString = mockMvc.perform(get("/reservations")
+                        .param("userId", userId.toString())
+                        .param("roomId", roomId.toString())
+                        .param("date", date.toString()))
                 .andExpect(status().isOk())
                 .andReturn()
                 .getResponse()
                 .getContentAsString();
         List<ReservationResponse> controllerResponse =
-                objectMapper.readValue(responseString, new TypeReference<List<ReservationResponse>>() {});
-
-        for (int i = 0; i < controllerResponse.size(); i++) {
-            assertThat(controllerResponse.get(i).id()).isEqualTo(serviceResponse.get(i).id());
-            assertThat(controllerResponse.get(i).userId()).isEqualTo(serviceResponse.get(i).userId());
-            assertThat(controllerResponse.get(i).roomId()).isEqualTo(serviceResponse.get(i).roomId());
-            assertThat(controllerResponse.get(i).startTime()).isEqualTo(serviceResponse.get(i).startTime());
-            assertThat(controllerResponse.get(i).endTime()).isEqualTo(serviceResponse.get(i).endTime());
-            assertThat(controllerResponse.get(i).reservationStatus()).isEqualTo(serviceResponse.get(i).reservationStatus());
-        }
+                objectMapper.readValue(responseString, new TypeReference<List<ReservationResponse>>() {
+                });
+        /* verify */
+        assertThat(controllerResponse)
+                .usingRecursiveComparison()
+                .isEqualTo(serviceResponse);
     }
 
     @Test
     void getReservationTest() throws Exception {
-        Long id = 1L;
-        Long userId = 11L;
-        Long roomId = 12L;
-        LocalDateTime startTime = LocalDateTime.of(2026, 3, 1, 10, 30, 0, 0);
-        LocalDateTime endTime = LocalDateTime.of(2026, 3, 1, 11, 30, 0, 0);
-        ReservationStatus status = ReservationStatus.RESERVATION_STATUS_ACTIVE;
-
-        when(reservationService.getReservation(id))
-                .thenReturn(new ReservationResponse(id, userId, roomId, startTime, endTime, status));
-
+        /* request */
+        Long id = 0L;
+        /* mock service response */
+        ReservationResponse serviceResponse = new ReservationResponse(id, 10L, 100L,
+                LocalDateTime.of(2026, 3, 1, 10, 30, 0, 0),
+                LocalDateTime.of(2026, 3, 1, 11, 30, 0, 0),
+                ReservationStatus.RESERVATION_STATUS_ACTIVE);
+        when(reservationService.getReservation(id)).thenReturn(serviceResponse);
+        /* perform */
         String responseString = mockMvc.perform(get("/reservations/{id}", id))
                 .andExpect(status().isOk())
                 .andReturn()
                 .getResponse()
                 .getContentAsString();
-        ReservationResponse response = objectMapper.readValue(responseString, ReservationResponse.class);
-
-        assertThat(response.userId()).isEqualTo(userId);
-        assertThat(response.roomId()).isEqualTo(roomId);
-        assertThat(response.startTime()).isEqualTo(startTime);
-        assertThat(response.endTime()).isEqualTo(endTime);
-        assertThat(response.reservationStatus()).isEqualTo(status);
+        ReservationResponse controllerResponse = objectMapper.readValue(responseString, ReservationResponse.class);
+        /* verify */
+        assertThat(controllerResponse)
+                .usingRecursiveComparison()
+                .isEqualTo(serviceResponse);
     }
 
     @Test
     void addReservationTest() throws Exception {
+        /* request */
         Long id = 0L;
         Long userId = 10L;
         Long roomId = 11L;
         LocalDateTime startTime = LocalDateTime.of(2026, 3, 1, 10, 30, 0, 0);
         LocalDateTime endTime = LocalDateTime.of(2026, 3, 1, 11, 30, 0, 0);
-
-        when(reservationService.addReservation(eq(userId), any(ReservationRequest.class)))
-                .thenReturn(new ReservationResponse(
-                        id, userId, roomId, startTime, endTime,
-                        ReservationStatus.RESERVATION_STATUS_ACTIVE));
-
+        /* mock service response */
+        ReservationResponse serviceResponse = new ReservationResponse(
+                id, userId, roomId, startTime, endTime,
+                ReservationStatus.RESERVATION_STATUS_ACTIVE);
+        when(reservationService.addReservation(eq(userId), any(ReservationRequest.class))).thenReturn(serviceResponse);
+        /* perform */
         String responseString = mockMvc.perform(post("/reservations")
                         .param("userId", userId.toString())
                         .contentType(MediaType.APPLICATION_JSON)
@@ -117,37 +123,36 @@ public class ReservationControllerTest {
                 .andReturn()
                 .getResponse()
                 .getContentAsString();
-        ReservationResponse response = objectMapper.readValue(responseString, ReservationResponse.class);
-
-        assertThat(response.userId()).isEqualTo(userId);
-        assertThat(response.roomId()).isEqualTo(roomId);
-        assertThat(response.startTime()).isEqualTo(startTime);
-        assertThat(response.endTime()).isEqualTo(endTime);
-        assertThat(response.reservationStatus()).isEqualTo(ReservationStatus.RESERVATION_STATUS_ACTIVE);
+        ReservationResponse controllerResponse = objectMapper.readValue(responseString, ReservationResponse.class);
+        /* verify */
+        assertThat(controllerResponse)
+                .usingRecursiveComparison()
+                .isEqualTo(serviceResponse);
     }
 
     @Test
     void deleteReservationTest() throws Exception {
+        /* request */
         Long id = 0L;
-
+        /* mock */
         doNothing().when(reservationService).deleteReservation(eq(id));
-
+        /* perform */
         mockMvc.perform(delete("/reservations/{id}", id))
                 .andExpect(status().isNoContent());
     }
 
     @Test
     void updateReservationTimeTest() throws Exception {
+        /* request */
         Long id = 0L;
-        Long userId = 10L;
-        Long roomId = 11L;
         LocalDateTime startTime = LocalDateTime.of(2026, 3, 1, 10, 30, 0, 0);
         LocalDateTime endTime = LocalDateTime.of(2026, 3, 1, 11, 30, 0, 0);
-
+        /* mock service response */
+        ReservationResponse serviceResponse = new ReservationResponse(id, 10L, 11L,
+                startTime, endTime, ReservationStatus.RESERVATION_STATUS_ACTIVE);
         when(reservationService.updateReservationTime(eq(id), any(TimeRangeRequest.class)))
-                .thenReturn(new ReservationResponse(id, userId, roomId, startTime, endTime,
-                ReservationStatus.RESERVATION_STATUS_ACTIVE));
-
+                .thenReturn(serviceResponse);
+        /* perform */
         String responseString = mockMvc.perform(put("/reservations/{id}", id)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(this.objectMapper.writeValueAsString(new TimeRangeRequest(startTime, endTime))))
@@ -155,13 +160,10 @@ public class ReservationControllerTest {
                 .andReturn()
                 .getResponse()
                 .getContentAsString();
-        ReservationResponse response = objectMapper.readValue(responseString, ReservationResponse.class);
-
-        assertThat(response.id()).isEqualTo(id);
-        assertThat(response.userId()).isEqualTo(userId);
-        assertThat(response.roomId()).isEqualTo(roomId);
-        assertThat(response.startTime()).isEqualTo(startTime);
-        assertThat(response.endTime()).isEqualTo(endTime);
-        assertThat(response.reservationStatus()).isEqualTo(ReservationStatus.RESERVATION_STATUS_ACTIVE);
+        ReservationResponse controllerResponse = objectMapper.readValue(responseString, ReservationResponse.class);
+        /* verify */
+        assertThat(controllerResponse)
+                .usingRecursiveComparison()
+                .isEqualTo(serviceResponse);
     }
 }

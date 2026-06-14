5a0989398993f5d26705ae1e4da2164214faa15e
zsjgithub654 <zsj654@gmail.com>
2026-06-14 00:38:35 -0700

    add error message in response body in GlobalExceptionHandler

--- src/main/java/com/zsj/RoomBooking/exception/GlobalExceptionHandler.java
+++ src/main/java/com/zsj/RoomBooking/exception/GlobalExceptionHandler.java
@@ -1,26 +1,35 @@
 package com.zsj.RoomBooking.exception;
 
+import com.zsj.RoomBooking.model.dto.response.ErrorResponse;
 import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-/* To convert ConstraintViolationException caused by validation errors to ResponseEntity */
+/* To return corresponding response for each exception */
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<Void> handleResourceNotFoundException(ResourceNotFoundException exception) {
-        return ResponseEntity.notFound().build();
+    public ResponseEntity<ErrorResponse> handleResourceNotFoundException(ResourceNotFoundException exception) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(new ErrorResponse(exception.getMessage()));
     }
 
     @ExceptionHandler({IllegalArgumentException.class, IllegalStateException.class})
-    public ResponseEntity<Void> handleIllegalArgumentAndStateException(RuntimeException exception) {
-        return ResponseEntity.badRequest().build();
+    public ResponseEntity<ErrorResponse> handleIllegalArgumentAndStateException(RuntimeException exception) {
+        return ResponseEntity
+                .badRequest()
+                .body(new ErrorResponse(exception.getMessage()));
     }
 
+    /* method parameter validation exception */
     @ExceptionHandler(ConstraintViolationException.class)
-    public ResponseEntity<Void> handleConstraintViolationException(ConstraintViolationException exception) {
-        return ResponseEntity.badRequest().build();
+    public ResponseEntity<ErrorResponse> handleConstraintViolationException(ConstraintViolationException exception) {
+        return ResponseEntity
+                .badRequest()
+                .body(new ErrorResponse(exception.getMessage()));
     }
 }

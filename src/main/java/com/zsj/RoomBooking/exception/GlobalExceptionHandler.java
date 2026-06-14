35d65fe5424b285ae76477c45566030e1e1b9d3f
zsjgithub654 <zsj654@gmail.com>
2026-06-13 19:32:01 -0700

    add exception handlers for ResourceNotFoundException, IllegalArgumentException

--- src/main/java/com/zsj/RoomBooking/exception/GlobalExceptionHandler.java
+++ src/main/java/com/zsj/RoomBooking/exception/GlobalExceptionHandler.java
@@ -5,8 +5,19 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+/* To convert ConstraintViolationException caused by validation errors to ResponseEntity */
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<Void> handleResourceNotFoundException(ResourceNotFoundException exception) {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler({IllegalArgumentException.class, IllegalStateException.class})
+    public ResponseEntity<Void> handleIllegalArgumentAndStateException(RuntimeException exception) {
+        return ResponseEntity.badRequest().build();
+    }
 
     @ExceptionHandler(ConstraintViolationException.class)
     public ResponseEntity<Void> handleConstraintViolationException(ConstraintViolationException exception) {
